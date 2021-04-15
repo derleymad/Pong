7d2e1654d9b63ad788d91cba401fefc47e007f03
Wanderson Valentim Tavares <wanderson200108@gmail.com>
2021-04-15 14:54:26 -0300

    adição das funções de movimentar a barra na classe jogador

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
 import sys
 import pygame
 from pygame.locals import *
 
-
+#Variáveis Globais
 loopdojogo = True
 
 branco = (255,255,255)
@@ -35,23 +34,40 @@ altura = 600
 
 tela = pygame.display.set_mode((largura,altura))
 
-# IMPORTA A BOLA 
-bola = pygame.image.load("bola2.png")
-bola = pygame.transform.scale(bola, (scale, scale))
-bolarect = bola.get_rect()
-
 class jogador():
-    def __init__(self, posX, posY, largura, altura):
+    def __init__(self, posX, posY, largura, altura, alturaInicial):
         self.xJogador = posX
         self.yJogador = posY
         self.largura = largura
         self.altura = altura
+        self.alturaInicial = alturaInicial
 
     def desenhaJogador(self, superficie, cor):
         pygame.draw.rect(superficie, cor, [self.xJogador, self.yJogador, self.largura, self.altura])
+    
+    def movimentacaoBarra1(self, alturaTotal):
+        keys = pygame.key.get_pressed()
+        if keys[K_w] and not self.alturaInicial <= 20:
+            self.yJogador -= 5
+            self.alturaInicial = self.alturaInicial - 5
+
+        if keys[K_s] and not self.alturaInicial >= alturaTotal-(self.altura+20):
+            self.yJogador += 5
+            self.alturaInicial = self.alturaInicial + 5
+        #placar(self.alturaInicial, 100, 100, branco)
+    
+    def movimentacaoBarra2(self, alturaTotal):
+        keys = pygame.key.get_pressed()
+        if keys[K_UP] and not self.alturaInicial <= 20:
+            self.yJogador -= 5
+            self.alturaInicial = self.alturaInicial - 5
+
+        if keys[K_DOWN] and not self.alturaInicial >= alturaTotal-(self.altura+20):
+            self.yJogador += 5
+            self.alturaInicial = self.alturaInicial + 5
+        #placar(self.alturaInicial, 200, 200, branco)
 
 
-# DESENHA PLACAR
 def placar(pontos, posX, posY, cor):
     num = str(pontos)
     fontesys = pygame.font.SysFont(None, 100)
@@ -63,11 +79,19 @@ def desenha_interface():
     pygame.draw.rect(tela, branco, [0, 0, largura, 20])
     pygame.draw.line(tela, branco, (largura/2,0), (largura/2,altura), 20)
 
+
+# IMPORTA A BOLA 
+bola = pygame.image.load("bola1.png")
+bola = pygame.transform.scale(bola, (scale, scale))
+bolarect = bola.get_rect()
+
 bolarect.center = (largura/2,altura/2)
 
-jogador1 = jogador(20, (altura/2)-40, 20, 80)
-jogador2 = jogador(largura-40, (altura/2)-40, 20, 80)
-jogador1Altura = (altura/2)-40
+alturaInicial = (altura/2)-40
+jogador1 = jogador(20, (altura/2)-40, 20, 80, alturaInicial)
+jogador2 = jogador(largura-40, (altura/2)-40, 20, 80, alturaInicial)
+
+numeroJogadores = 2
 
 while loopdojogo:
     
@@ -80,31 +104,21 @@ while loopdojogo:
     jogador1.desenhaJogador(tela, branco)
     jogador2.desenhaJogador(tela, branco)
 
-    #MOVIMENTAÇÃO DA BARRA
-    keys = pygame.key.get_pressed()
-    if keys[K_w] and not jogador1Altura <= 20:
-        jogador1.yJogador -= 5
-        jogador1Altura = jogador1Altura - 5
+    if numeroJogadores == 1:
+        jogador1.movimentacaoBarra1(altura)
+        #movimentação barra bot
+    else:
+        jogador1.movimentacaoBarra1(altura)
+        jogador2.movimentacaoBarra2(altura)
 
-    if keys[K_s] and not jogador1Altura >= altura-(jogador1.altura+20):
-        jogador1.yJogador += 5
-        jogador1Altura = jogador1Altura + 5
-
-    '''if bolarect.left <= 40 and bolarect.top >= jogador1Altura and bolarect.bottom <= jogador1Altura + jogador1.altura:
+    '''if bolarect.left <= 40 and bolarect.top >= jogador1Altura and bolarect.bottom <= alturaInicial + jogador1.altura:
         velocidade[0] = -velocidade[0]
     
-    #if
-    if bolarect.right < 40 and bolarect.bottom > jogador1Altura: 
+    if bolarect.right < 40 and bolarect.bottom > alturaInicial: 
         velocidade[1] = -velocidade[1]
     placar(jogador1Altura, 100, 100, branco)'''
 
 
-
-    #desenhaJogador()
-    #if jogador == 2: jogador2 = pygame.draw.rect(tela, branco, [largura-40, altura/2-40, 20, 80])
-    #jogador1 = pygame.draw.rect(tela, branco, [20, altura/2-40, 20, 80])
-    #jogador2 = pygame.draw.rect(tela, branco, [largura-40, altura/2-40, 20, 80])
-    
     #PLACAR
     if pontuacaoJogador1 >= 10:
         placar(pontuacaoJogador1, largura/2-100, 30, branco)
@@ -115,6 +129,7 @@ while loopdojogo:
     bolarect = bolarect.move(velocidade)
 
 
+    #Verifica se a bola saiu, adiciona o ponto e centraliza a bola
     if bolarect.left < -scale:
         pontuacaoJogador2 += 1
         bolarect.center = (largura/2,altura/2)
@@ -123,19 +138,8 @@ while loopdojogo:
     if bolarect.right > largura + scale:
         pontuacaoJogador1 += 1
         bolarect.center = (largura/2,altura/2)
-        velocidade[0] = -velocidadePadrao
-    
+        velocidade[0] = -velocidadePadrao    
 
-
-    '''if bolarect.left < -scale or bolarect.right > largura+scale:
-        if verifica == 1:
-            velocidade[0]*-1
-            verifica = 0
-        else:
-            velocidade[0]*1
-            verifica = 1
-        bolarect.center = (largura/2,altura/2)'''
-        
 
     #if bolarect.left < 0 or bolarect.right > largura+30:
     #    bolarect.center = (largura/2,altura/2)
